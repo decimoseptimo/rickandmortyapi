17389821338b2b3f3907f05fbb8c44c97dd69df5
Miguel Valenzuela <decimoseptimo@gmail.com>
2021-05-05 18:47:35 -0700

    add characterView tests & mock gatsby link
    A setup has to be done to support testing in gatsby, see:
    https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/#3-useful-mocks-to-complete-your-testing-environment

---
+++ src/components/character/characterView.test.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+
+import CharacterView from "./characterView"
+
+const data = {
+  id: "test id",
+  name: "test name",
+  image: "test image",
+  status: "test status",
+  gender: "test gender",
+  origin: { name: "test origin" },
+  location: { name: "test location" },
+}
+
+it("renders correctly", () => {
+  render(<CharacterView data={data} />)
+
+  // image
+  expect(screen.getByAltText(data.name)).toBeInTheDocument()
+  // name
+  expect(screen.getByRole("heading", { name: data.name })).toBeInTheDocument()
+  // X Y from Z
+  expect(
+    screen.getByText(
+      `${data.status} ${data.gender.toLowerCase()} from ${data.origin.name}`
+    )
+  ).toBeInTheDocument()
+  // last seen
+  expect(
+    screen.getByText(data.location.name, { exact: false })
+  ).toBeInTheDocument()
+  // update
+  const updateBtn = screen.getByRole("button", { name: /update character/i })
+  expect(updateBtn).toBeInTheDocument()
+  expect(updateBtn.closest("a")).toHaveAttribute(
+    "href",
+    `/update/?id=${data.id}`
+  )
+  // delete
+  const deleteBtn = screen.getByRole("button", { name: /delete character/i })
+  expect(deleteBtn).toBeInTheDocument()
+  expect(deleteBtn.closest("a")).toHaveAttribute(
+    "href",
+    `/delete/?id=${data.id}`
+  )
+})
+
+it("renders with fallback image", () => {
+  render(<CharacterView data={{}} />)
+
+  // undefined fields are going to be presented as "undefined" to the user
+  // we'll only assert that a fallback image is displayed
+  expect(screen.getByRole("img")).toHaveAttribute("src", "https://via.placeholder.com/400?text=Not%20found")
+})
--- src/components/statusIcon.js
+++ src/components/statusIcon.js
@@ -1,6 +1,10 @@
 import React from "react"
 
-// TODO: validate status
+/**
+ * Renders colored dot according to given state
+ * @param {*} props 
+ * @returns ReactElement 
+ */
 export default function StatusIcon({ status }) {
-  return <span className={`status-icon ${status.toLowerCase()}`}></span>
+  return <span className={`status-icon ${status?.toLowerCase()}`}></span>
 }
