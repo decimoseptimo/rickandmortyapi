4874e65959fca77ef0d04c0c46a83ac7a54cbbf1
Miguel Valenzuela <decimoseptimo@gmail.com>
2021-05-24 18:23:48 -0700

    Add 'reload' button to header.js
    refactor header
    refactor main.test.js

--- cypress/e2e/main.test.js
+++ cypress/e2e/main.test.js
@@ -1,22 +1,11 @@
+import { getRandomInt } from "../../src/utils"
+
 /**
  * Finds all the character cards in the current page and count them
  * set the count as @initialCardsCount alias,
  * chooses one randomly and set it as @randomCard alias
  */
 function setup() {
-  /**
-   * Returns a random integer between min (inclusive) and max (inclusive).
-   * The value is no lower than min (or the next integer greater than min
-   * if min isn't an integer) and no greater than max (or the next integer
-   * lower than max if max isn't an integer).
-   * Using Math.round() will give you a non-uniform distribution!
-   */
-  function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  }
-
   cy.findAllByTestId("characterCard").then(el => {
     cy.wrap(el.length).as("initialCardsCount")
     const index = getRandomInt(0, el.length - 1)
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { Center, Flex, Box } from "@chakra-ui/react"
+import { Flex, Box, Button } from "@chakra-ui/react"
 import About from "./about"
+
+import { getRandomInt } from "../utils"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -11,26 +13,38 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
-    <Center>
-      <Flex align={`center`}>
-        <Box py="4" px="1">
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            >
-              {siteTitle}
-            </Link>
-          </h1>
-        </Box>
-        <Box py="4" px="1">
-          <About />
-        </Box>
-      </Flex>
-    </Center>
+    <Flex align={`center`} justifyContent="center">
+      <Box py="4" px="1">
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </Box>
+      <Box py="4" px="1">
+        <About />
+      </Box>
+      <Button
+        colorScheme="teal"
+        size="xs"
+        variant="link"
+        css={{
+          position: "absolute",
+          right: "1rem",
+          color: "#471c71",
+          textTransform: "uppercase",
+        }}
+        onClick={() => console.log(getRandomInt(0, 9))}
+      >
+        Reload
+      </Button>
+    </Flex>
   </header>
 )
 
